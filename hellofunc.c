2a587c3b76272af0a9c33cf9d8f2aee58e8d5d85
Yasmine <yasminedp5@gmail.com>
2018-10-10 15:02:44 -0400

    Assignment 4 completed

--- hellofunc.c
+++ hellofunc.c
@@ -1,6 +1,8 @@
 Copyright 2018 <Robert Phillips>
 #include <stdio.h>
 #include <hellomake.h>
+
+//Good job of following the given directions -yasmine
 void myPrintHelloMake(void) {
   printf("Hello makefiles!\n");
   return;
